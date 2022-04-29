f25632e68047881b1333f835c7f6efbdaaad75b3
Tita Wijayanti <wijayantitita094@gmail.com>
2022-04-29 20:47:25 +0700

    Source Code Praktikum dan Tugas minggu ke-10

---
+++ Praktikum 10/react-firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCOFPzHWlCn1xQRLZM8WxC_wkn1zO7S2nA",
+    authDomain: "react-firebase-64e9e.firebaseapp.com",
+    projectId: "react-firebase-64e9e",
+    storageBucket: "react-firebase-64e9e.appspot.com",
+    messagingSenderId: "657983664620",
+    appId: "1:657983664620:web:e9f9ea96f52339d498d0c9",
+    measurementId: "G-X1RJ5G0MYW"
+}
